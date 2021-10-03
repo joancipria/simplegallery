32c7cc3a3b9022d308f137c6c91ed14de575d11a
Joan Ciprià <joancipria@protonmail.com>
2021-10-03 20:22:41 +0200

    Get home dir

--- src/scanner.rs
+++ src/scanner.rs
@@ -4,9 +4,16 @@ use std::vec::Vec;
 
 pub fn get_images() -> Vec<String> {
 
+    let mut homePath:String;
+
+    match home::home_dir() {
+        Some(path) => homePath = path.display().to_string()+"/Pictures",//println!("{}", path.display()),
+        None => homePath="".to_string(),//println!("Impossible to get your home dir!"),
+    }
+
     let mut images:Vec<String> = Vec::new();
 
-    let paths = fs::read_dir("/home/joan/Pictures").unwrap();
+    let paths = fs::read_dir(homePath).unwrap();
 
     for path in paths {
         //println!("Name: {}", path.unwrap().path().display())
